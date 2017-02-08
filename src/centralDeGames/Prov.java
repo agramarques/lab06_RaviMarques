eb54a8ada5f8c00c7102c85b8dc2cd06aab8033d
Ravi Marques <Ravi Marques>
2017-02-08 16:04:29 -0300

    Classe provisoria com main para observar comportamento do sistema

---
+++ src/centralDeGames/Prov.java
@@ -0,0 +1,17 @@
+package centralDeGames;
+
+public class Prov {
+
+	public static void main(String[] args) {
+		Loja minha = new Loja();
+		Usuario u = new Noob("Ravi");
+		u.setLogin("ravi");
+		Jogo j = new Jogo("first", TiposDeJogo.Luta);
+		j.setPreco(200);
+		minha.adicionaUsuario(u);
+		minha.creditaUsuario(u.getLogin(), 1000);
+		minha.venderJogo(u.getLogin(), j);
+		System.out.println(minha);
+	}
+
+}
