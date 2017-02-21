ce047b85b2124c8ad7cddc871907e7f42eb5bb51
agramarques <agramarques@gmail.com>
2017-02-21 09:50:18 -0300

    facade

---
+++ src/centralDeGames/LojaFacade.java
@@ -0,0 +1,39 @@
+package centralDeGames;
+
+public class LojaFacade {
+
+	private LojaController controller;
+
+	public void adicionaUsuario(Usuario u) {
+		controller.adicionaUsuario(u);
+	}
+
+	public void creditaUsuario(String login, double valor) {
+		controller.creditaUsuario(login, valor);
+	}
+
+	public Usuario criaUsuario(String nome, String login, String nivel) {
+		return controller.criaUsuario(nome, login, nivel);
+	}
+
+	public Jogo criaJogo(String nome, double preco, TiposDeJogo tipo, String modos) {
+		return controller.criaJogo(nome, preco, tipo, modos);
+	}
+
+	public void venderJogo(String login, String nome, double preco, TiposDeJogo tipo) throws Exception {
+		controller.venderJogo(login, nome, preco, tipo);
+	}
+
+	public Usuario buscaUsuario(String login) {
+		return controller.buscaUsuario(login);
+	}
+
+	public void upgradeUsuario(Usuario user) throws Exception {
+		controller.upgradeUsuario(user);
+	}
+
+	public String toString() {
+		return controller.toString();
+	}
+	
+}
